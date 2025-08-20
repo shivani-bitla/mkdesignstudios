bf040e74acfa93075720e3d09883fd71bf880d19
shivani <sbitla719@gmail.com>
2025-08-20 11:00:37 +0530

    deleted files

--- backend/src/main/java/com/mkdesignstudios/backend/repository/UserProfileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mkdesignstudios.backend.repository;
-
-import com.mkdesignstudios.backend.model.UserProfile;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.Optional;
-
-public interface UserProfileRepository extends MongoRepository<UserProfile, String> {
-    Optional<UserProfile> findByUsername(String username);
-}
